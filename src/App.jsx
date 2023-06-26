094b1a43196b5cfd8c239bc81de4469114a142bd
MRHemel <hemelrahman80@gmail.com>
2023-06-26 23:59:12 +0600

    navbar and banner section done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Banner from './components/Banner'
+import Navbar from './components/Navbar'
+
+function App() {
+
+
+  return (
+    <div className='max-w-screen-xl		mx-auto'>
+      <Navbar></Navbar>
+      <Banner></Banner>
+    </div>
+  )
+}
+
+export default App
